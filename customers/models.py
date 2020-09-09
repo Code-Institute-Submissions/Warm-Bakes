19a45d854682f49ceba078afc1084efef046399a
l0nelyhermit <mrjonathangoh@gmail.com>
2020-09-09 17:27:57 +0800

    added customer model to customer app

--- customers/models.py
+++ customers/models.py
@@ -1,3 +1,13 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from phonenumber_field.modelfields import PhoneNumberField
 # Create your models here.
+
+class Customer(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    contact_number = PhoneNumberField(null=False,blank=False,unique=True)
+    address = models.CharField(max_length=100,null=True,blank=False)
+    postal_code = models.PositiveIntegerField(unique=True,blank=False)
+    def __str__(self):
+        return self.user.username
+
