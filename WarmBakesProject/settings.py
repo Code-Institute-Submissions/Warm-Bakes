736104faf4495b2c08b81f4c35846ea5a02dae88
l0nelyhermit <mrjonathangoh@gmail.com>
2020-09-09 17:06:38 +0800

    created apps for products,lessons,reviews and customers

--- WarmBakesProject/settings.py
+++ WarmBakesProject/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -38,6 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Personal Applications
+    'customers',
+    'products',
+    'lessons',
+    'reviews',
 ]
 
 MIDDLEWARE = [
