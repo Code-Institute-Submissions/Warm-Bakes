eee4a14e6ab70a6b3ab1f1203f79b1ba27c532de
l0nelyhermit <mrjonathangoh@gmail.com>
2020-09-09 17:21:10 +0800

    added difficulty and lesson models to lessons app in models.py

--- lessons/models.py
+++ lessons/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+class Difficulty(models.Model):
+    name= models.CharField(max_length=255,blank=False)
+
+    def __str__(self):
+        return self.name
+
+class Lesson(models.Model):
+    name = models.CharField(max_length=255,blank=False)
+    difficulty_level = models.ForeignKey(Difficulty,on_delete=models.SET_NULL,null=True)
+    description = models.TextField(blank=False)
+    average_class_size = models.PositiveIntegerField(blank=False)
+    price = models.PositiveIntegerField(blank=False)
+
+
+    def __str__(self):
+        return self.name
+
+    
