1eea1e64e47e012475a0ef11b896e68638fecf24
l0nelyhermit <mrjonathangoh@gmail.com>
2020-09-14 02:30:07 +0800

    updated profile template page, added login_required together with flash  messages to customers views and fixed urls.py for customers app

--- customers/urls.py
+++ customers/urls.py
@@ -3,5 +3,5 @@ import customers.views
 
 urlpatterns=[
     path("myprofile/",customers.views.show_user_profile,name="profile_page_route"),
-    path("profile/update/<user_id>",customers.views.update_profile,name="update_profile_page_route")
+    path("myprofile/update/",customers.views.update_profile,name="update_profile_page_route")
 ]
--- customers/views.py
+++ customers/views.py
@@ -1,23 +1,27 @@
 from django.shortcuts import render,redirect,get_object_or_404
 from .models import Customer
 from .forms import CustomerProfileForm
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
+@login_required
 def show_user_profile(request):
-    user_profile=get_object_or_404(Customer,pk=request.user)
+    user_profile=get_object_or_404(Customer,user=request.user)
     return render(request,'customers/show_profile.template.html',{
     'profile': user_profile
     })
-
-def update_profile(request,user_id):
-    user_to_update = get_object_or_404(Customer,pk=user_id)
+@login_required
+def update_profile(request):
+    user_to_update = get_object_or_404(Customer,user=request.user)
 
     if request.method == "POST":
         customer_form = CustomerProfileForm(request.POST, instance=user_to_update)
 
         if customer_form.is_valid():
             customer_form.save()
+            messages.success(request,f"Customer profile has been updated")
             return redirect(show_user_profile)
         else:
             return render(request, 'customers/update_profile.template.html',{
